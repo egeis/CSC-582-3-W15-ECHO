f50897a2118590f0da36640d314409fd217d20ee
egeis <rcoan@callutheran.edu>
2015-02-06 16:05:20 -0800

    Simple Echo Return for testing EXEC()

---
+++ src/com/echo/test/Echo.java
@@ -0,0 +1,16 @@
+package com.echo.test;
+
+/**
+ *
+ * @author Richard Coan
+ */
+public class Echo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Echo");
+    }
+    
+}
