1501502147a904a601b8db23841f7d4878e971e0
egeis <rcoan@callutheran.edu>
2015-02-06 18:24:48 -0800

    Update Node code to handle sending and recieving messages.  Does not currently do anything with those recieved.

--- src/com/echo/Node.java
+++ src/com/echo/Node.java
@@ -21,52 +21,102 @@ public class Node {
     private ServerSocket server;
     private ObjectInputStream input;
     private ObjectOutputStream output;
-        
     private boolean shutdown = false;
+    private int parent = -1;
+    private int id = -1;
+    private int port = -1;
     
-    private static ArrayList<Integer> adj = new ArrayList<Integer>();
+    public static String TIMER_HOST = "localhost";
+    public static int TIMER_PORT = 1212;
+        
+    protected static ArrayList<Conn> adj = new ArrayList<Conn>();
     
-    private Node(int port)
+    private Node()
     {        
-        try {
-            server = new ServerSocket(port);
+        //Get an ID
+        try ( 
+            Socket socket = new Socket(TIMER_HOST, TIMER_PORT)) {
+            output = new ObjectOutputStream(socket.getOutputStream());
+            output.writeInt(1);
+            output.flush();
             
-            while(true)
-            {
-                Packet p = getMessage();
-                if(shutdown) break;
-            }
-                        
-            server.close();
-        } catch (IOException | ClassNotFoundException ex) {
-            LOGGER.log(Level.SEVERE, null, ex);
+            input = new ObjectInputStream(socket.getInputStream());
+            id = input.readInt();
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            acceptMessages();
         }
     }
     
-    private Packet getMessage() throws IOException, ClassNotFoundException
+    private void acceptMessages()
     {
-        Socket socket = server.accept();
-        input = new ObjectInputStream(socket.getInputStream());
-        Packet p = (Packet) input.readObject();
-        this.parsePacket(p);                
-        socket.close();
+        try {
+            server = new ServerSocket(port);
+        } catch (IOException ex) {
+            LOGGER.log(Level.SEVERE, null, ex);
+        }
         
-        return p;
+        while(true)
+        {
+            try (Socket socket = server.accept()) {
+                input = new ObjectInputStream(socket.getInputStream());
+                Packet p = (Packet) input.readObject();
+                this.parsePacket(p);
+            } catch (IOException | ClassNotFoundException ex) {
+                LOGGER.log(Level.WARNING, null, ex);
+            } 
+            
+            if(shutdown) break;
+        }      
+        
+        try {
+            server.close();
+        } catch (IOException ex) {
+            LOGGER.log(Level.WARNING, null, ex);
+        }
+    }
+    
+     /**
+     * Sends a packet of data to the initiator.
+     * @param p the packet.
+     */
+    private void sendPacket(Packet p)
+    {        
+        try {
+            for(Conn c : adj) 
+            {
+                if(c.id == this.parent) continue;
+                
+                Socket out = new Socket(c.host, c.port);    
+                output = new ObjectOutputStream(out.getOutputStream()); 
+                output.writeObject(p);
+                output.flush();
+                out.close();
+            }
+        } catch (IOException ex) {
+           LOGGER.log(Level.SEVERE, null, ex);
+        }
     }
     
     private void parsePacket(Packet p)
     {        
         switch(p.type) 
         {
-            case PacketHelper.TYPE_BROADCAST:
+            case PacketHelper.BROADCAST:
                 break;
-            case PacketHelper.TYPE_REPLY:
+            case PacketHelper.REPLY:
                 break;
-            case PacketHelper.TYPE_SHUTDOWN:
+            case PacketHelper.WAVE_BROADCAST:
+                break;
+            case PacketHelper.WAVE_REPLY:
+                break;
+            case PacketHelper.SHUTDOWN:
                 shutdown = true;
                 break;
             default:
-                       
+                
         }
     }
     
@@ -75,7 +125,7 @@ public class Node {
         int port = 0;
         int id = 0;
         
-        if(args.length > 2)
+        if(args.length > 2 )
         {
             //Setup Port and ID.
             port = Integer.parseInt(args[0]);
@@ -85,7 +135,13 @@ public class Node {
             //Get Adjacent List
             for(int i = 2; i < args.length; i++)
             {
-                adj.add(Integer.parseInt(args[i]));
+                String[] parts = args[i].split(":");
+                                
+                Conn c = new Conn();
+                c.host = parts[0];
+                c.port = Integer.parseInt(parts[1]);
+                
+                adj.add(c);
             }
         } 
         else 
@@ -94,7 +150,19 @@ public class Node {
             System.exit(1);     //Something Went Wrong...
         }
         
-        Node node = new Node(port);
+        Node node = new Node();
         System.out.println("Exiting...");
+    }
+    
+    protected static class Conn 
+    {
+        public String host;
+        public int port;
+        public int id = 0;
+        
+        public String toString()
+        {
+            return "[Host:"+host+", Port:"+port+"]";
+        }
     }
 }
